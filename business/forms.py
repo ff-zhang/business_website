8ffe37535055f6f731ff7173d73c4ce75d6e3981
Felix Zhang <38085633+Hogwarts250@users.noreply.github.com>
2020-07-28 09:38:28 -0700

    fixed super() init error

--- business/forms.py
+++ business/forms.py
@@ -9,12 +9,12 @@ class ClubForm(forms.ModelForm):
 
 class EmailForm(forms.Form):
     first_name = forms.CharField(max_length=20)
-    recipient = forms.EmailField()
+    email = forms.EmailField()
 
     # Choices is empty to avoid migration error
     options = forms.MultipleChoiceField(choices=[], widget=forms.CheckboxSelectMultiple)
 
     def __init__(self, *args, **kwargs):
         club_names = [(club.name.lower(), club.name) for club in Club.objects.order_by("name")]
-        super.__init__(*args, **kwargs)
-        self.field["options"].choices = club_names
+        super().__init__(*args, **kwargs)
+        self.fields["options"].choices = club_names
