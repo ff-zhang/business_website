4dbea009e9a9f92feb28d92c259da30b8c04236a
Felix Zhang <38085633+Hogwarts250@users.noreply.github.com>
2020-09-22 16:47:31 -0700

    update email conf

--- website/settings/heroku.py
+++ website/settings/heroku.py
@@ -16,28 +16,30 @@ env = environ.Env(
 )
 
 # False if not in os.environ
-DEBUG = env('DEBUG')
+DEBUG = env("DEBUG")
 
-# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
-SECRET_KEY = env('SECRET_KEY')
+# Raises django"s ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(", ")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(", ")
 
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
-    # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
-    'default': env.db(),
+    # read os.environ["DATABASE_URL"] and raises ImproperlyConfigured exception if not found
+    "default": env.db(),
 }
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SENDGRID_API_KEY = env("SENDGRID_API_KEY")
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.sendgrid.net"
 
 EMAIL_USE_TLS = True
 
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'felixzhangtest@gmail.com'
+EMAIL_HOST_USER = "apikey"
 
-EMAIL_HOST_PASSWORD = 'e0kqVrmN'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
